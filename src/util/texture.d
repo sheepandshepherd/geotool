3b714c36c27e90307e75acd73a5efdf01335ddcd
sheepandshepherd <sheepandshepherd@hotmail.com>
2016-05-29 03:13:23 +0200

    Add blank 1x1 texture for use in UI

--- src/util/texture.d
+++ src/util/texture.d
@@ -6,6 +6,24 @@ import derelict.devil.ilu, derelict.devil.ilut;
 import util.linalg;
 import gl3n.linalg;
 
+__gshared private uint _blankTexture = 0;
+@property @nogc nothrow
+uint blankTexture()
+{
+	if(_blankTexture == 0)
+	{
+		glGenTextures(1,&_blankTexture);
+		glBindTexture(GL_TEXTURE_2D, _blankTexture);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		ubyte zero = 0;
+		glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA8,1,1,0,GL_RGBA,GL_UNSIGNED_BYTE,&zero);
+	}
+	return _blankTexture;
+}
+
 
 private bool ilProcessErrors(string tag = null)
 {
