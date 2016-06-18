91d7c55718123f4bf3b6794d9e8bdd169f950520
sheepandshepherd <sheepandshepherd@hotmail.com>
2016-06-18 17:44:32 +0200

    Add LRRish UI theme

--- src/ui.d
+++ src/ui.d
@@ -783,7 +783,6 @@ static:
 
 		newFrame();
 
-
 		/// FPS bar at bottom
 		if(showDebug && Map.tiles !is null && Map.sortedMeshes !is null && Map.sortedMeshes.length > 0)
 		{
@@ -820,6 +819,55 @@ static:
 
 
 		igRender();
+	}
+
+	void setLRRStyle(ImGuiStyle* style)
+	{
+		style.Colors[ImGuiCol_Text]                  = ImVec4(0.90f, 0.90f, 0.90f, 1.00f);
+		style.Colors[ImGuiCol_TextDisabled]          = ImVec4(0.60f, 0.60f, 0.60f, 1.00f);
+		style.Colors[ImGuiCol_WindowBg]              = ImVec4(0.03f, 0.07f, 0.00f, 0.47f);
+		style.Colors[ImGuiCol_ChildWindowBg]         = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+		style.Colors[ImGuiCol_PopupBg]               = ImVec4(0.05f, 0.09f, 0.10f, 0.90f);
+		style.Colors[ImGuiCol_Border]                = ImVec4(0.00f, 0.39f, 0.00f, 0.86f);
+		style.Colors[ImGuiCol_BorderShadow]          = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+		style.Colors[ImGuiCol_FrameBg]               = ImVec4(0.80f, 0.80f, 0.80f, 0.30f);
+		style.Colors[ImGuiCol_FrameBgHovered]        = ImVec4(0.80f, 0.90f, 0.80f, 0.40f);
+		style.Colors[ImGuiCol_FrameBgActive]         = ImVec4(0.65f, 0.90f, 0.65f, 0.45f);
+		style.Colors[ImGuiCol_TitleBg]               = ImVec4(0.45f, 0.90f, 1.00f, 0.45f);
+		style.Colors[ImGuiCol_TitleBgCollapsed]      = ImVec4(0.36f, 0.72f, 0.80f, 0.20f);
+		style.Colors[ImGuiCol_TitleBgActive]         = ImVec4(0.45f, 0.90f, 1.00f, 0.55f);
+		style.Colors[ImGuiCol_MenuBarBg]             = ImVec4(0.40f, 0.52f, 0.55f, 0.80f);
+		style.Colors[ImGuiCol_ScrollbarBg]           = ImVec4(0.20f, 0.25f, 0.30f, 0.60f);
+		style.Colors[ImGuiCol_ScrollbarGrab]         = ImVec4(0.40f, 0.73f, 0.80f, 0.30f);
+		style.Colors[ImGuiCol_ScrollbarGrabHovered]  = ImVec4(0.40f, 0.73f, 0.80f, 0.40f);
+		style.Colors[ImGuiCol_ScrollbarGrabActive]   = ImVec4(0.88f, 0.83f, 0.00f, 0.50f);
+		style.Colors[ImGuiCol_ComboBg]               = ImVec4(0.20f, 0.20f, 0.20f, 0.99f);
+		style.Colors[ImGuiCol_CheckMark]             = ImVec4(0.00f, 1.00f, 0.00f, 0.55f);
+		style.Colors[ImGuiCol_SliderGrab]            = ImVec4(1.00f, 1.00f, 1.00f, 0.30f);
+		style.Colors[ImGuiCol_SliderGrabActive]      = ImVec4(0.00f, 1.00f, 0.00f, 0.55f);
+		style.Colors[ImGuiCol_Button]                = ImVec4(0.21f, 0.71f, 0.21f, 0.60f);
+		style.Colors[ImGuiCol_ButtonHovered]         = ImVec4(0.21f, 0.71f, 0.21f, 1.00f);
+		style.Colors[ImGuiCol_ButtonActive]          = ImVec4(0.38f, 0.78f, 0.38f, 1.00f);
+		style.Colors[ImGuiCol_Header]                = ImVec4(0.37f, 0.80f, 0.90f, 0.45f);
+		style.Colors[ImGuiCol_HeaderHovered]         = ImVec4(0.42f, 0.81f, 0.90f, 0.80f);
+		style.Colors[ImGuiCol_HeaderActive]          = ImVec4(0.53f, 0.81f, 0.87f, 0.80f);
+		style.Colors[ImGuiCol_Column]                = ImVec4(0.50f, 0.50f, 0.50f, 1.00f);
+		style.Colors[ImGuiCol_ColumnHovered]         = ImVec4(0.70f, 0.60f, 0.60f, 1.00f);
+		style.Colors[ImGuiCol_ColumnActive]          = ImVec4(0.90f, 0.70f, 0.70f, 1.00f);
+		style.Colors[ImGuiCol_ResizeGrip]            = ImVec4(1.00f, 1.00f, 1.00f, 0.30f);
+		style.Colors[ImGuiCol_ResizeGripHovered]     = ImVec4(1.00f, 1.00f, 1.00f, 0.60f);
+		style.Colors[ImGuiCol_ResizeGripActive]      = ImVec4(1.00f, 1.00f, 1.00f, 0.90f);
+		style.Colors[ImGuiCol_CloseButton]           = ImVec4(0.90f, 0.44f, 0.44f, 0.50f);
+		style.Colors[ImGuiCol_CloseButtonHovered]    = ImVec4(0.90f, 0.30f, 0.30f, 0.60f);
+		style.Colors[ImGuiCol_CloseButtonActive]     = ImVec4(0.70f, 0.70f, 0.70f, 1.00f);
+		style.Colors[ImGuiCol_PlotLines]             = ImVec4(1.00f, 1.00f, 1.00f, 1.00f);
+		style.Colors[ImGuiCol_PlotLinesHovered]      = ImVec4(0.90f, 0.70f, 0.00f, 1.00f);
+		style.Colors[ImGuiCol_PlotHistogram]         = ImVec4(0.90f, 0.70f, 0.00f, 1.00f);
+		style.Colors[ImGuiCol_PlotHistogramHovered]  = ImVec4(1.00f, 0.60f, 0.00f, 1.00f);
+		style.Colors[ImGuiCol_TextSelectedBg]        = ImVec4(0.00f, 1.00f, 0.35f, 0.35f);
+		style.Colors[ImGuiCol_ModalWindowDarkening]  = ImVec4(0.20f, 0.20f, 0.20f, 0.35f);
+		
+
 	}
 
 
@@ -1017,6 +1065,8 @@ THE SOFTWARE.`;
 			glfwSetKeyCallback(window, &keyCallback);
 			glfwSetCharCallback(window, &charCallback);
 		}
+
+		setLRRStyle(igGetStyle());
 	}
 	
 	void createDeviceObjects()
