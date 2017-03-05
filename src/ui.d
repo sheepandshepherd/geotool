aacb000c6c920e95e875f433907467d7f1b792ef
sheepandshepherd <sheepandshepherd@hotmail.com>
2017-03-05 07:35:14 +0100

    Clean up deprecated string concatenation

--- src/ui.d
+++ src/ui.d
@@ -1228,30 +1228,34 @@ THE SOFTWARE.`;
 	void createDeviceObjects()
 	{
 		const GLchar *vertex_shader =
-			"#version 330\n"
-				"uniform mat4 ProjMtx;\n"
-				"in vec2 Position;\n"
-				"in vec2 UV;\n"
-				"in vec4 Color;\n"
-				"out vec2 Frag_UV;\n"
-				"out vec4 Frag_Color;\n"
-				"void main()\n"
-				"{\n"
-				"	Frag_UV = UV;\n"
-				"	Frag_Color = Color;\n"
-				"	gl_Position = ProjMtx * vec4(Position.xy,0,1);\n"
-				"}\n";
+		q{
+			#version 330
+				uniform mat4 ProjMtx;
+				in vec2 Position;
+				in vec2 UV;
+				in vec4 Color;
+				out vec2 Frag_UV;
+				out vec4 Frag_Color;
+				void main()
+				{
+					Frag_UV = UV;
+					Frag_Color = Color;
+					gl_Position = ProjMtx * vec4(Position.xy,0,1);
+			}
+		};
 		
 		const GLchar* fragment_shader =
-			"#version 330\n"
-				"uniform sampler2D Texture;\n"
-				"in vec2 Frag_UV;\n"
-				"in vec4 Frag_Color;\n"
-				"out vec4 Out_Color;\n"
-				"void main()\n"
-				"{\n"
-				"	Out_Color = Frag_Color * texture( Texture, Frag_UV.st);\n"
-				"}\n";
+		q{
+			#version 330
+				uniform sampler2D Texture;
+				in vec2 Frag_UV;
+				in vec4 Frag_Color;
+				out vec4 Out_Color;
+				void main()
+				{
+					Out_Color = Frag_Color * texture( Texture, Frag_UV.st);
+				}
+		};
 		
 		g_ShaderHandle = glCreateProgram();
 		g_VertHandle = glCreateShader(GL_VERTEX_SHADER);
